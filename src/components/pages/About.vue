cc2f42a2d7f1ac67cc410effd521ed7437a3a8b2
Lê Trần Anh Vũ <vule5726@gmail.com>
2019-03-08 10:32:45 +0700

    laywer feature

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -1,12 +1,19 @@
 <template>
   <section>
     <h1>About page</h1>
+    <storage-input></storage-input>
+
   </section>
 </template>
 
 <script>
+import StorageInput from '../storage/StorageInput'
+
 export default {
-  name: 'About'
+  name: 'About',
+  components: {
+    'storage-input': StorageInput
+  }
 }
 </script>
 
---
+++ src/components/storage/StorageInput.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <!--<input class="cus-input" type="text" v-model="dummyfile">-->
+    <div class="input-group mb-3">
+      <input type="text" class="form-control" v-model="fileName">
+      <div class="input-group-append">
+        <span class="input-group-text" id="basic-addon2">{{extension}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StorageInput',
+  data () {
+    return {
+      dummyfile: 'abcyxz.23.234.23.dfa-sjhdfssddddfa-sds.png',
+      folder: {}
+    }
+  },
+  computed: {
+    extension () {
+      return this.dummyfile.split('.').slice(-1)[0]
+    },
+    fileName () {
+      const restLen = this.dummyfile.length - this.extension.length
+      return this.dummyfile.substring(0, restLen - 1)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .cus-input{
+    width: 400px
+  }
+
+</style>
